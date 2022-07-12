c9cbf61c58e105ee2d6579b8dc21b0b4be2a2bfe
Gafarli Tutu <qeferlitutu@gmail.com>
2022-07-12 23:34:40 +0400

    changeable text and rotate effect

--- script.js
+++ script.js
@@ -12,3 +12,43 @@ $(".choosing-title").on("click", function () {
   $(this).addClass("active");
 });
 
+
+const words = ['Creative', 'Jobo'];
+let i = 0;
+let timer;
+
+function typingEffect() {
+    let word = words[i].split("");
+    var loopTyping = function() {
+        if (word.length > 0) {
+            document.querySelector('.changeable-text').innerHTML += word.shift();
+        } else {
+            deletingEffect();
+            return false;
+        };
+        timer = setTimeout(loopTyping, 250);
+    };
+    loopTyping();
+};
+
+function deletingEffect() {
+  let word = words[i].split("");
+  var loopDeleting = function() {
+      if (word.length > 0) {
+          word.pop();
+          document.querySelector('.changeable-text').innerHTML = word.join("");
+      } else {
+          if (words.length > (i + 1)) {
+              i++;
+          } else {
+              i = 0;
+          };
+          typingEffect();
+          return false;
+      };
+      timer = setTimeout(loopDeleting, 200);
+  };
+  loopDeleting();
+};
+
+typingEffect();
